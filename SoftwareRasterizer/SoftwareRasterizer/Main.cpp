46dc571f2091ff10512239683783625e550e6b12
Mukund <ykmukund@gmai,com>
2012-10-02 00:00:52 +0530

    removed experimental glm code

--- SoftwareRasterizer/SoftwareRasterizer/Main.cpp
+++ SoftwareRasterizer/SoftwareRasterizer/Main.cpp
@@ -40,14 +40,8 @@ void initialiseGLUT(int argc, char **argv)
 }
 
 int main(int argc, char **argv)
-{
-	glm::vec4 Position = glm::vec4(glm::vec3(0.0), 1.0);
-	glm::mat4 Model = glm::mat4(1.0);
-	Model[3] = glm::vec4(1.0, 1.0, 0.0, 1.0);
-	glm::vec4 Transformed = Model * Position;	
-
+{	
 	initialiseGLUT(argc, argv);		
-
 	return 0;
 }
 
